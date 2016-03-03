3e160bfe318020a2fa545ac73d7e20989598b66c
Stanislav Yotov <yotov.st@gmail.com>
2016-03-03 14:35:36 +0000

    Initall commit for FizzBuzz

--- src/FuzzBuzz.java
+++ src/FuzzBuzz.java
@@ -1,4 +1,9 @@
 
 public class FuzzBuzz {
-
+	private static String[] fizzBuzz = {"","fizz","buzz","fizzbuzz"};
+	public static void main(String[] argv){
+		for (int i=0; i<100;i++){
+			System.out.println(fizzBuzz[ (i%3==0?1:0) + (i%5==0?2:0)] + i);	
+		}
+	}
 }

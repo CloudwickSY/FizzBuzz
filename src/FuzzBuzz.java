35bf1e6012d1210af15835c8bde08942c3bb83c3
Stanislav Yotov <yotov.st@gmail.com>
2016-03-03 15:02:15 +0000

    added comments

--- src/FuzzBuzz.java
+++ src/FuzzBuzz.java
@@ -1,12 +1,31 @@
 
-
+/**
+ * FuzzBuzz class is a class that contains an implementation of the fizzBuzz algorithm 
+ * For multiples of three print “Fizz” instead of the number
+ * For the multiples of five print “Buzz”.
+ * For numbers which are multiples of both three and five print “FizzBuzz”.
+ * @author Stanislav
+ *
+ */
 public class FuzzBuzz {
 	private static String[] fizzBuzz = {"","Fizz","Buzz","Fizzbuzz"};
+	/**
+	 * 
+	 * @param number the value for which we need to compute fizzBuzz
+	 * @return
+	 */
 	public String version1(int number) {
-		return fizzBuzz[(number%3==0?1:0)+(number%5==0?2:0)];
+		String result = fizzBuzz[(number%3==0?1:0)+(number%5==0?2:0)];
+		return result.isEmpty()?(number+""):result;
 	}
-	public String version2(int number) {
-		return (number%3==0?"Fizz":"")+(number%5==0?"Buzz":"")+number;
+	/**
+	 * 
+	 * @param number the value for which we need to compute fizzBuzz
+	 * @return the fizzBuzz string for the input
+	 */
+	public String version2(int number) { 
+		String result = (number%3==0?"Fizz":"")+(number%5==0?"Buzz":"");
+		return result.isEmpty()?(number+""):result;
 	}
 }
 

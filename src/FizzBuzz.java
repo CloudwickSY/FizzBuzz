6c46240a3f81b11ddaf2400cd262850a44de099c
Stanislav Yotov <yotov.st@gmail.com>
2016-03-04 11:42:36 +0000

    Indented the code

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,29 +1,34 @@
 /**
- * FuzzBuzz class is a class that contains an implementation of the fizzBuzz algorithm 
- * For multiples of three print “Fizz” instead of the number
- * For the multiples of five print “Buzz”.
- * For numbers which are multiples of both three and five print “FizzBuzz”.
+ * FuzzBuzz class is a class that contains an implementation of the fizzBuzz
+ * algorithm For multiples of three print “Fizz” instead of the number For the
+ * multiples of five print “Buzz”. For numbers which are multiples of both three
+ * and five print “FizzBuzz”.
+ * 
  * @author Stanislav
  *
  */
 public class FizzBuzz {
-	private static String[] fizzBuzz = {"","Fizz","Buzz","FizzBuzz"};
+	private static String[] fizzBuzz = { "", "Fizz", "Buzz", "FizzBuzz" };
+
 	/**
 	 * 
-	 * @param number the value for which we need to compute fizzBuzz
+	 * @param number
+	 *            the value for which we need to compute fizzBuzz
 	 * @return the fizzBuzz string for the input
 	 */
 	public String version1(int number) {
-		String result = fizzBuzz[(number%3==0?1:0)+(number%5==0?2:0)];
-		return result.isEmpty()?(number+""):result;
+		String result = fizzBuzz[(number % 3 == 0 ? 1 : 0) + (number % 5 == 0 ? 2 : 0)];
+		return result.isEmpty() ? (number + "") : result;
 	}
+
 	/**
 	 * 
-	 * @param number the value for which we need to compute fizzBuzz
+	 * @param number
+	 *            the value for which we need to compute fizzBuzz
 	 * @return the fizzBuzz string for the input
 	 */
-	public String version2(int number) { 
-		String result = (number%3==0?"Fizz":"")+(number%5==0?"Buzz":"");
-		return result.isEmpty()?(number+""):result;
+	public String version2(int number) {
+		String result = (number % 3 == 0 ? "Fizz" : "") + (number % 5 == 0 ? "Buzz" : "");
+		return result.isEmpty() ? (number + "") : result;
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,27 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] argv){
-		FizzBuzz  fuzzBuzz = new FizzBuzz ();
-		for (int number=1; number<=100;number++){
+	public static void main(String[] argv) {
+		FizzBuzz fuzzBuzz = new FizzBuzz();
+		for (int number = 1; number <= 100; number++) {
 			System.out.println(fuzzBuzz.version1(number));
 		}
-		for (int number=1; number<=100;number++){
+		for (int number = 1; number <= 100; number++) {
 			System.out.println(fuzzBuzz.version2(number));
 		}
-		
+
 		/* Create a scanner so that we can read from the user */
 		System.out.println("Please provide with a positive intiger.\nNegative intiger will terminate the loop");
 		Scanner in = new Scanner(System.in);
-		int num = in.nextInt();//To do: Verify user input
-		while (num>0){//Iterate until the user gives a negative number
+		int num = in.nextInt();// To do: Verify user input
+		while (num > 0) {// Iterate until the user gives a negative number
 			System.out.println(fuzzBuzz.version2(num));
-			System.out.println("~~~~~~~~~~~~~\nPlease provide with a positive intiger.\nNegative intiger will terminate the loop"); 
+			System.out.println(
+					"~~~~~~~~~~~~~\nPlease provide with a positive intiger.\nNegative intiger will terminate the loop");
 			num = in.nextInt();
 		}
 		in.close();
 		System.out.println("Thank you for playing. Please come again.");
-		
+
 	}
 }
